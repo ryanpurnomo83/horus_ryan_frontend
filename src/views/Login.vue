11011f326b27867b7b90a76c41e0b231acf096ca
ryapurnomo83 <ryanpurnomo83@gmail.com>
2025-11-07 04:16:01 +0000

    third commit

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,11 +1,23 @@
 <template>
-    <h1>Login</h1>
+    <img src="../assets/horus-logo-small.png" className="flex align-items items-center justify-center mb-4">
     <div className="flex flex-col gap-4 mb-10">
         <label>Username: <input type="text" className="border"/></label>
         <label>Password: <input type="password" className="border"/></label>
     </div>
-    <div className="flex flex-row gap-5">
+    <div className="flex flex-row gap-5 align-items items-center justify-center">
         <button>Login</button>
-        <button>Register</button>
+        <button @click="$router.push('/register')">Register</button>
     </div>
 </template>
+
+<!--
+<script>
+    export default{
+        name: 'Login',
+        methods: {
+            goToRegister(){
+                this.$router.push({ name: 'Register' });
+            }
+        }
+    }
+</script>-->
--- src/views/Register.vue
+++ src/views/Register.vue
@@ -1,9 +1,13 @@
 <template>
-    <h1>Login</h1>
-    <label>Username: <input type="text"/></label>
-    <label>Password: <input type="password"/></label>
-    <div>
-        <button>Login</button>
-        <button>Register</button>
+    <img src="../assets/horus-logo-small.png" className="flex align-items items-center justify-center mb-4">
+    <div className="flex flex-col gap-4 mb-10">
+        <label>Nama Lengkap: <input type="text" className="border"/></label>
+        <label>Email: <input type="email" className="border"/></label>
+        <label>Username: <input type="text" className="border"/></label>
+        <label>Password: <input type="password" className="border"/></label>
+    </div>
+    <div className="flex flex-row gap-5 align-items items-center justify-center">
+        <button @click="$router.push('/login')">Login</button>
+        <button @click="">Register</button>
     </div>
 </template>
